28d32cf17e002d956d5601e8a33d350e4862f9b6
Petr Broz <petr.broz@autodesk.com>
2022-11-21 11:03:13 +0100

    Removed upload capabilities...
    ... to simplify the code base. This simplified app will be used as the basis for other code samples related to the viewer.

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -3,7 +3,6 @@ import { initViewer, loadModel } from './viewer.js';
 initViewer(document.getElementById('preview')).then(viewer => {
     const urn = window.location.hash?.substring(1);
     setupModelSelection(viewer, urn);
-    setupModelUpload(viewer);
 });
 
 async function setupModelSelection(viewer, selectedUrn) {
@@ -26,83 +25,7 @@ async function setupModelSelection(viewer, selectedUrn) {
     }
 }
 
-async function setupModelUpload(viewer) {
-    const upload = document.getElementById('upload');
-    const input = document.getElementById('input');
-    const models = document.getElementById('models');
-    upload.onclick = () => input.click();
-    input.onchange = async () => {
-        const file = input.files[0];
-        let data = new FormData();
-        data.append('model-file', file);
-        if (file.name.endsWith('.zip')) { // When uploading a zip file, ask for the main design file in the archive
-            const entrypoint = window.prompt('Please enter the filename of the main design inside the archive.');
-            data.append('model-zip-entrypoint', entrypoint);
-        }
-        upload.setAttribute('disabled', 'true');
-        models.setAttribute('disabled', 'true');
-        showNotification(`Uploading model <em>${file.name}</em>. Do not reload the page.`);
-        try {
-            const resp = await fetch('/api/models', { method: 'POST', body: data });
-            if (!resp.ok) {
-                throw new Error(await resp.text());
-            }
-            const model = await resp.json();
-            setupModelSelection(viewer, model.urn);
-        } catch (err) {
-            alert(`Could not upload model ${file.name}. See the console for more details.`);
-            console.error(err);
-        } finally {
-            clearNotification();
-            upload.removeAttribute('disabled');
-            models.removeAttribute('disabled');
-            input.value = '';
-        }
-    };
-}
-
 async function onModelSelected(viewer, urn) {
-    if (window.onModelSelectedTimeout) {
-        clearTimeout(window.onModelSelectedTimeout);
-        delete window.onModelSelectedTimeout;
-    }
     window.location.hash = urn;
-    try {
-        const resp = await fetch(`/api/models/${urn}/status`);
-        if (!resp.ok) {
-            throw new Error(await resp.text());
-        }
-        const status = await resp.json();
-        switch (status.status) {
-            case 'n/a':
-                showNotification(`Model has not been translated.`);
-                break;
-            case 'inprogress':
-                showNotification(`Model is being translated (${status.progress})...`);
-                window.onModelSelectedTimeout = setTimeout(onModelSelected, 5000, viewer, urn);
-                break;
-            case 'failed':
-                showNotification(`Translation failed. <ul>${status.messages.map(msg => `<li>${JSON.stringify(msg)}</li>`).join('')}</ul>`);
-                break;
-            default:
-                clearNotification();
-                loadModel(viewer, urn);
-                break; 
-        }
-    } catch (err) {
-        alert('Could not load model. See the console for more details.');
-        console.error(err);
-    }
-}
-
-function showNotification(message) {
-    const overlay = document.getElementById('overlay');
-    overlay.innerHTML = `<div class="notification">${message}</div>`;
-    overlay.style.display = 'flex';
-}
-
-function clearNotification() {
-    const overlay = document.getElementById('overlay');
-    overlay.innerHTML = '';
-    overlay.style.display = 'none';
+    loadModel(viewer, urn);
 }
