36b7aaee9c16abc697f931314ca3ad05111054e5
Petr Broz <petr.broz@autodesk.com>
2022-11-29 12:00:02 +0100

    Added markup screenshot sample.

--- wwwroot/main.js
+++ wwwroot/main.js
@@ -1,8 +1,40 @@
 import { initViewer, loadModel } from './viewer.js';
 
+function download(href, name) {
+    const link = document.createElement('a');
+    link.download = name;
+    link.style.opacity = 0;
+    document.body.append(link);
+    link.href = href;
+    link.click();
+    link.remove();
+}
+
+function generateScreenshot(viewer) {
+    const screenshot = new Image();
+    screenshot.onload = function () {
+        const canvas = document.createElement('canvas');
+        canvas.width = viewer.container.clientWidth;
+        canvas.height = viewer.container.clientHeight;
+        const ctx = canvas.getContext('2d');
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+        ctx.drawImage(screenshot, 0, 0, canvas.width, canvas.height);
+        const markupCoreExt = viewer.getExtension('Autodesk.Viewing.MarkupsCore');
+        markupCoreExt.renderToCanvas(ctx, function () {
+            download(canvas.toDataURL(), 'screenshot.png');
+        });
+    };
+    viewer.getScreenShot(viewer.container.clientWidth, viewer.container.clientHeight, function (blob) {
+        screenshot.src = blob;
+    });
+}
+
 initViewer(document.getElementById('preview')).then(viewer => {
     const urn = window.location.hash?.substring(1);
     setupModelSelection(viewer, urn);
+    document.getElementById('save-markups').addEventListener('click', function () {
+        generateScreenshot(viewer);
+    });
 });
 
 async function setupModelSelection(viewer, selectedUrn) {
