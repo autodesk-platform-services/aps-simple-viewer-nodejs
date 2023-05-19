e47df21002c7180c9e8f0afd6d2ff824eb7e7394
Petr Broz <petr.broz@autodesk.com>
2023-05-19 10:53:32 +0200

    Removed model uploading and status checking.

--- services/aps.js
+++ services/aps.js
@@ -1,4 +1,3 @@
-const fs = require('fs');
 const APS = require('forge-apis');
 const { APS_CLIENT_ID, APS_CLIENT_SECRET, APS_BUCKET } = require('../config.js');
 
@@ -21,20 +20,7 @@ service.getPublicToken = async () => {
     return publicAuthClient.getCredentials();
 };
 
-service.ensureBucketExists = async (bucketKey) => {
-    try {
-        await new APS.BucketsApi().getBucketDetails(bucketKey, null, await service.getInternalToken());
-    } catch (err) {
-        if (err.response.status === 404) {
-            await new APS.BucketsApi().createBucket({ bucketKey, policyKey: 'temporary' }, {}, null, await service.getInternalToken());
-        } else {
-            throw err;
-        }
-    }
-};
-
 service.listObjects = async () => {
-    await service.ensureBucketExists(APS_BUCKET);
     let resp = await new APS.ObjectsApi().getObjects(APS_BUCKET, { limit: 64 }, null, await service.getInternalToken());
     let objects = resp.body.items;
     while (resp.body.next) {
@@ -43,49 +29,6 @@ service.listObjects = async () => {
         objects = objects.concat(resp.body.items);
     }
     return objects;
-};
-
-service.uploadObject = async (objectName, filePath) => {
-    await service.ensureBucketExists(APS_BUCKET);
-    const buffer = await fs.promises.readFile(filePath);
-    const results = await new APS.ObjectsApi().uploadResources(
-        APS_BUCKET,
-        [{ objectKey: objectName, data: buffer }],
-        { useAcceleration: false, minutesExpiration: 15 },
-        null,
-        await service.getInternalToken()
-    );
-    if (results[0].error) {
-        throw results[0].completed;
-    } else {
-        return results[0].completed;
-    }
-};
-
-service.translateObject = async (urn, rootFilename) => {
-    const job = {
-        input: { urn },
-        output: { formats: [{ type: 'svf', views: ['2d', '3d'] }] }
-    };
-    if (rootFilename) {
-        job.input.compressedUrn = true;
-        job.input.rootFilename = rootFilename;
-    }
-    const resp = await new APS.DerivativesApi().translate(job, {}, null, await service.getInternalToken());
-    return resp.body;
-};
-
-service.getManifest = async (urn) => {
-    try {
-        const resp = await new APS.DerivativesApi().getManifest(urn, {}, null, await service.getInternalToken());
-        return resp.body;
-    } catch (err) {
-        if (err.response.status === 404) {
-            return null;
-        } else {
-            throw err;
-        }
-    }
 };
 
 service.urnify = (id) => Buffer.from(id).toString('base64').replace(/=/g, '');
